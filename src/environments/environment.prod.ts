c11fc67952b493c00f2f4f0c5594ff5e0f87ea35
John Nguyen <john12052003@gmail.com>
2024-09-04 22:06:46 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    BASE_URL: 'http://localhost:5187',
+    BASE_URL: 'https://taskslayer-backend-428633081c3b.herokuapp.com',
     CLIENT_ID: '701050554727-25he92do85jdchlftnhd4nr3sai8jutv.apps.googleusercontent.com',
     };
